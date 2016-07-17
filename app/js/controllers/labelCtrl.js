11245fe1803cd820d8826bbd878a6e20223bedcd
Hervé BREDIN <bredin@limsi.fr>
2016-07-17 17:57:39 +0200

    wip: try to fix video player

--- app/js/controllers/labelCtrl.js
+++ app/js/controllers/labelCtrl.js
@@ -40,9 +40,9 @@ angular.module('myApp.controllers')
                 document.getElementById('videoTest1').innerHTML = "";
                 document.getElementById('videoTest2').innerHTML = "";
                 document.getElementById('videoTest3').innerHTML = "";
+
                 // Get queue first element and pop it from the queue
                 camomileService.dequeue($scope.model.incomingQueue, function (err, item) {
-
 
                     if (err) {
                         alert(item.error);
@@ -55,13 +55,11 @@ angular.module('myApp.controllers')
                         return;
                     }
 
-                    //$scope.model.resetTransparentPlan();
                     $scope.model.updateIsDisplayedVideo(true);
 
                     $scope.model.q = item;
-                    //document.getElementById('label_name').innerHTML = $scope.model.q.person_name;
                     $('#shotImage').tooltip({title:$scope.model.q.person_name, placement:"left"});
-                    document.getElementById('shot').src = "/static/"+$scope.model.q.person_name+".png";
+                    document.getElementById('shot').src = "/static/" + $scope.model.q.person_name + ".png";
                     $scope.model.initialData = $scope.model.q.person_name;
 
                     // Update the add entry button's status
@@ -82,22 +80,12 @@ angular.module('myApp.controllers')
                             $scope.model.serverDate = results.serverDate;
                             $scope.model.clientDate = Date.now();
 
-                            $scope.model.restrict_toggle = 2;
-                            $scope.model.current_time_temp = $scope.model.q.start;
-                            var delta = $scope.model.q.end - $scope.model.q.start;
                             var offset = 0.2;
-                            $scope.model.infbndsec = parseFloat($scope.model.q.start+offset || 0);
-                            if ($scope.model.infbndsec < 0) {
-                                $scope.model.infbndsec = 0;
-                            }
-                            $scope.model.supbndsec = parseFloat($scope.model.q.end-offset || 0);
-                            if ($scope.model.supbndsec > $scope.model.fullDuration) {
-                                $scope.model.supbndsec = $scope.model.fullDuration;
-                            }
-                            $scope.model.duration = $scope.model.supbndsec - $scope.model.infbndsec;
+                            $scope.model.infbndsec = parseFloat($scope.model.q.start + offset || 0);
+                            $scope.model.supbndsec = parseFloat($scope.model.q.end - offset || 0);
 
                             $scope.$apply(function () {
-                                $scope.model.current_time = $scope.model.q.start+offset;
+                                $scope.model.current_time = $scope.model.infbndsec;
                             });
 
                         });
@@ -113,9 +101,6 @@ angular.module('myApp.controllers')
                 item.log.user = Session.username;
                 item.log.date = $scope.model.serverDate;
                 item.log.duration = Date.now() - $scope.model.clientDate;
-                /*camomileService.me(function(callback){
-                    item.log.user_id = _id;
-                });*/
 
                 item.input = {};
                 item.input.shot_id = $scope.model.q.shot_id;
@@ -143,88 +128,5 @@ angular.module('myApp.controllers')
 
                 });
             };
-
-            $document.on(
-                "keyup",
-                function (event) {
-                    var targetID = event.target.id;
-                    var button_checked = false;
-                    if (targetID == 'confirm' || targetID == 'cancel') {
-                        button_checked = true;
-                    }
-                    //enter
-                    if (event.keyCode == 13 && targetID != 'localServerInput') {
-                        //If the focus is on the check buttons, blur the focus first
-                        if (button_checked) {
-                            event.target.blur();
-                        }
-                        $scope.$apply(function () {
-                            $scope.model.saveQueueElement("talking_face");
-                        });
-                    }
-                    //space
-                    if (event.keyCode == 32) {
-                        if (button_checked) {
-                            event.target.blur();
-                        }
-                        $scope.$apply(function () {
-                            $scope.model.toggle_play();
-                        });
-                    }
-                    //esc-> skip
-                    if (event.keyCode == 27) {
-                        $scope.$apply(function () {
-                            //skip
-                            //$scope.model.saveQueueElement(false);
-                        });
-                    }
-                });
-            $document.on(
-                "keydown",
-                function (event) {
-                    //Left
-                    if (event.keyCode == 37) {
-                        $scope.$apply(function () {
-                            if ($scope.model.current_time - 0.04 > $scope.model.infbndsec) {
-                                $scope.model.current_time = $scope.model.current_time - 0.04;
-                            } else {
-                                $scope.model.current_time = $scope.model.infbndsec;
-                            }
-                        });
-                    }
-                    //Right
-                    if (event.keyCode == 39) {
-                        $scope.$apply(function () {
-                            if ($scope.model.current_time + 0.04 < $scope.model.supbndsec) {
-                                $scope.model.current_time = $scope.model.current_time + 0.04;
-                            } else {
-                                $scope.model.current_time = $scope.model.supbndsec;
-                            }
-                        });
-                    }
-                    //Up
-                    if (event.keyCode == 38) {
-                        $scope.$apply(function () {
-                            if ($scope.model.current_time - 1 > $scope.model.infbndsec) {
-                                $scope.model.current_time = $scope.model.current_time - 1;
-                            } else {
-                                $scope.model.current_time = $scope.model.infbndsec;
-                            }
-                        });
-
-                    } //Down
-                    if (event.keyCode == 40) {
-                        $scope.$apply(function () {
-
-                            if ($scope.model.current_time + 1 < $scope.model.supbndsec) {
-                                $scope.model.current_time = $scope.model.current_time + 1;
-                            } else {
-                                $scope.model.current_time = $scope.model.supbndsec;
-                            }
-                        });
-                    }
-                }
-            );
-
         }
     ]);
